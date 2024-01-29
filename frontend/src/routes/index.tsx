454fedcba7f55e6891a30990d6eb039c91a7a91a
Teemu Silver <53870133+Tembero11@users.noreply.github.com>
2024-01-29 17:36:35 +0000

    Improve search ui

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,19 +1,47 @@
-import { Container, FormControl, HStack, Input, Tag } from "@chakra-ui/react";
+import { CloseIcon, SearchIcon } from "@chakra-ui/icons";
+import {
+  Container,
+  HStack,
+  Input,
+  InputGroup,
+  InputRightElement,
+  Tag,
+  TagLabel,
+  TagRightIcon,
+} from "@chakra-ui/react";
 
 export default function IndexPage() {
   const filters = ["Men", "Women", "Free", "Always Open"];
 
   return (
     <Container m={0}>
-      <HStack>
-        <Input/>
-        <HStack spacing={4} justifyContent="start">
-          {filters.map((label) => (
-            <Tag key={label} size="lg" colorScheme="blue">
-              {label}
-            </Tag>
-          ))}
-        </HStack>
+      <HStack
+        position="absolute"
+        top={4}
+        left={4}
+        spacing={4}
+        justifyContent="start"
+        width="calc(100% - var(--chakra-space-4))"
+      >
+        <InputGroup maxW="250px">
+          <Input placeholder="Search" />
+          <InputRightElement>
+            <SearchIcon />
+          </InputRightElement>
+        </InputGroup>
+        {filters.map((label) => (
+          <Tag
+            key={label}
+            size="lg"
+            variant="subtle"
+            colorScheme="cyan"
+            userSelect="none"
+            cursor="pointer"
+          >
+            <TagLabel>{label}</TagLabel>
+            <TagRightIcon boxSize="10px" as={CloseIcon} />
+          </Tag>
+        ))}
       </HStack>
       <Container></Container>
     </Container>
