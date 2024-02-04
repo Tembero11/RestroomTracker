bb7302314088c60c48492336b9710fd85b3bf7bc
romppabear <ronjakarhu536@gmail.com>
2024-02-04 20:21:41 +0200

    Add required annotations

--- backend/src/main/java/io/github/tembero11/restroom/RestroomApplication.java
+++ backend/src/main/java/io/github/tembero11/restroom/RestroomApplication.java
@@ -2,10 +2,16 @@ package io.github.tembero11.restroom;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@EnableJpaRepositories("io.github.tembero.*")
+@ComponentScan(basePackages = { "io.github.tembero.*" })
+@EntityScan("io.github.tembero.*")   
 @SpringBootApplication
 @RestController
 public class RestroomApplication {
