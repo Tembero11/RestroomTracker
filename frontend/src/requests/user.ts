9733fa3c11ffa012158e73bd7929a9851f939ebd
Tembero11 <silver.teemu@gmail.com>
2024-03-01 01:40:10 +0200

    Add getUser api call

---
+++ frontend/src/requests/user.ts
@@ -0,0 +1,28 @@
+interface IUser {
+  username: string;
+  email: string;
+  isComplete: boolean;
+  lastLogin: Date;
+}
+
+type IRawUser = IUser & {lastLogin: string}
+
+export async function getUser(): Promise<IUser> {
+  const url = `/api/user`;
+  const res = await fetch(url, {
+    method: "get"
+  });
+
+  if (!res.ok) {
+    throw new Error("Unknown Error");
+  }
+
+  const body: IRawUser = await res.json();
+
+  return {
+    username: body.username,
+    email: body.email,
+    isComplete: body.isComplete,
+    lastLogin: new Date(body.lastLogin),
+  }
+}
