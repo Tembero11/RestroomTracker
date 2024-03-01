5abc2e161d168e5c6e072b6cb48a99f7979cfd6c
Tembero11 <silver.teemu@gmail.com>
2024-03-01 05:54:17 +0200

    Add map overlay

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,10 +1,22 @@
 import MapView from "../components/MapView/MapView";
+import { AuthStatus } from "../contexts/AuthContext/AuthContext";
+import useAuth from "../hooks/useAuth";
+import styles from "../styles/routes/index.module.scss";
 
 export default function IndexPage() {
+  const auth = useAuth();
+
   return (
     <div>
-      <MapView/>
-      <div></div>
+      <MapView />
+      <div className={styles["map-top-overlay"]}>
+        <input type="search" />
+        {auth.status == AuthStatus.authenticated ? (
+          <button>New</button>
+        ) : (
+          <button>Login</button>
+        )}
+      </div>
     </div>
   );
 }
