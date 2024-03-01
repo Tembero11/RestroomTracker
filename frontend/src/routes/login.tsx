d2c18b1fd49aa69f4e3e0e5ef102b27a2159ace4
Tembero11 <silver.teemu@gmail.com>
2024-03-01 02:01:11 +0200

    Add buttons to services

--- frontend/src/routes/login.tsx
+++ frontend/src/routes/login.tsx
@@ -1,5 +1,9 @@
 export default function LoginPage() {
     return (
-        <div></div>
+        <div>
+            <h1>Choose your login strategy</h1>
+            <a href={(import.meta as any).env.VITE_DISCORD_OAUTH2_LINK}><button>Discord</button></a>
+            <a href=""><button>Google</button></a>
+        </div>
     )
 }
