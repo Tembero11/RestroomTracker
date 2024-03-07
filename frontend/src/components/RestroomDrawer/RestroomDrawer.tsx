b6ab5a11cbca1ca3b1b76405afcbe88ad1e7ec0d
Tembero11 <silver.teemu@gmail.com>
2024-03-07 17:00:47 +0200

    Add more ui to drawer

--- frontend/src/components/RestroomDrawer/RestroomDrawer.tsx
+++ frontend/src/components/RestroomDrawer/RestroomDrawer.tsx
@@ -1,6 +1,6 @@
-import { CircularProgress, Drawer } from "@mui/material";
+import { Chip, CircularProgress, Drawer, Grid, Stack, Typography } from "@mui/material";
 import useApi from "../../hooks/useApi";
-import { getRestroomById } from "../../requests/restroom";
+import { Sex, getRestroomById } from "../../requests/restroom";
 import { useEffect } from "react";
 import { useSearchParams } from "react-router-dom";
 
@@ -24,6 +24,25 @@ export default function RestroomDrawer({
     setSearchParams(urlSearchParams);
   }, [id]);
 
+
+  function sexChips() {
+    switch (data?.sex) {
+      case Sex.Both:
+        return (
+          <>
+            <Chip label="Men"/>
+            <Chip label="Women"/>
+          </>
+        )
+      case Sex.Men:
+          return <Chip label="Men"/>
+      case Sex.Women:
+          return <Chip label="Women"/>
+      default:
+        return <></>
+    }
+  }
+  console.log(data)
   return (
     <Drawer anchor={"left"} open={id != null} onClose={() => {
       // Clear the id from the url bar
@@ -32,7 +51,20 @@ export default function RestroomDrawer({
     }}>
       {
         loading ? <CircularProgress/> : (
-          <span>Name: {data?.name}</span>
+          <Stack padding="16px" gap="8px" maxWidth="400px">
+            <Typography variant="h4">{data?.name}</Typography>
+            <Stack direction="row" gap="8px">
+              {sexChips()}
+            </Stack>
+            <Stack direction="row" justifyContent="space-between">
+              <Typography>Fee</Typography>
+              <Typography>{data?.fee?.toFixed(2)}€</Typography>
+            </Stack>
+            <Stack direction="row" justifyContent="space-between">
+              <Typography>Accessible</Typography>
+            </Stack>
+            <Typography variant="body1">{data?.notes}</Typography>
+          </Stack>
         )
       }
     </Drawer>
