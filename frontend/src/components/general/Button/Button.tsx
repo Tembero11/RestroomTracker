de1f8fedeb250e46a356544e52bf01a6ac09ebca
Tembero11 <silver.teemu@gmail.com>
2024-03-20 18:38:24 +0200

    Add button component

---
+++ frontend/src/components/general/Button/Button.tsx
@@ -0,0 +1,24 @@
+import styles from "./Button.module.scss";
+
+interface IProps
+  extends React.DetailedHTMLProps<
+    React.ButtonHTMLAttributes<HTMLButtonElement>,
+    HTMLButtonElement
+  > {
+  variant?: "primary" | "secondary";
+  isFloating?: boolean;
+}
+
+export default function Button(props: IProps) {
+  const variant = props.variant ?? "primary";
+
+  return (
+    <button
+      {...props}
+      className={styles[`button-${variant}`]}
+      style={{
+        boxShadow: props.isFloating ? "0px 5px 5px rgba(0, 0, 0, 0.3)" : undefined,
+      }}
+    />
+  );
+}
