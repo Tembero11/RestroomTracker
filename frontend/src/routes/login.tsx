8edaf2390721f9e46bd2d102de40720759e785fa
Tembero11 <silver.teemu@gmail.com>
2024-03-22 13:44:57 +0200

    Make use of custom components

--- frontend/src/routes/login.tsx
+++ frontend/src/routes/login.tsx
@@ -1,9 +1,18 @@
+import Button from "../components/general/Button/Button";
+import Card from "../components/general/Card/Card";
+import { Center, VStack } from "../components/general/Stack/Stack";
+
 export default function LoginPage() {
-    return (
-        <div>
-            <h1>Choose your login strategy</h1>
-            <a href={(import.meta as any).env.VITE_DISCORD_OAUTH2_LINK}><button>Discord</button></a>
-            <a href=""><button>Google</button></a>
-        </div>
-    )
+  return (
+    <Center h="100vh">
+      <Card>
+        <h1>Choose your login strategy</h1>
+        <VStack alignItems="stretch" w="100%">
+          <a href={(import.meta as any).env.VITE_DISCORD_OAUTH2_LINK}>
+            <Button variant="secondary" style={{width: "100%"}}>Discord</Button>
+          </a>
+        </VStack>
+      </Card>
+    </Center>
+  );
 }
